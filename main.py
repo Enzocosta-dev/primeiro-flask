af5d4039d0cfd8b8e69d785ff6aa639e87089c3a
Enzocosta-dev <enzocosta.dev@gmail.com>
2026-04-01 23:41:23 -0300

    commit dois

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 #rotas
-@app.route("/")
-def home():
-    return "hello world"
+from views import *
 
 if __name__ == "__main__" :
     app.run()
---
+++ views.py
@@ -0,0 +1,7 @@
+from main import app
+from flask import Flask, render_template
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
